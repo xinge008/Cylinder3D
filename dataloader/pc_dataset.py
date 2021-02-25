7df59464c9b1828f9f97084997b3d47beda612cd
xinge008 <zhuxinge321@gmail.com>
2021-02-25 17:14:50 +0800

    update data processing of multiscan semantic segmentation in SemanticKITTI

--- dataloader/pc_dataset.py
+++ dataloader/pc_dataset.py
@@ -125,6 +125,189 @@ def SemKITTI2train_single(label):
     label[remove_ind] = 255
     return label
 
+from os.path import join
+@register_dataset
+class SemKITTI_sk_multiscan(data.Dataset):
+    def __init__(self, data_path, imageset='train',return_ref=False, label_mapping="semantic-kitti-multiscan.yaml"):
+        self.return_ref = return_ref
+        with open(label_mapping, 'r') as stream:
+            semkittiyaml = yaml.safe_load(stream)
+        self.learning_map = semkittiyaml['learning_map']
+        self.imageset = imageset
+        self.data_path = data_path
+        if imageset == 'train':
+            split = semkittiyaml['split']['train']
+        elif imageset == 'val':
+            split = semkittiyaml['split']['valid']
+        elif imageset == 'test':
+            split = semkittiyaml['split']['test']
+        else:
+            raise Exception('Split must be train/val/test')
+
+        multiscan = 2 # additional two frames are fused with target-frame. Hence, 3 point clouds in total
+        self.multiscan = multiscan
+        self.im_idx = []
+
+        self.calibrations = []
+        self.times = []
+        self.poses = []
+
+        self.load_calib_poses()
+
+        for i_folder in split:
+            self.im_idx += absoluteFilePaths('/'.join([data_path, str(i_folder).zfill(2), 'velodyne']))
+
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.im_idx)
+
+    def load_calib_poses(self):
+        """
+        load calib poses and times.
+        """
+
+        ###########
+        # Load data
+        ###########
+
+        self.calibrations = []
+        self.times = []
+        self.poses = []
+
+        for seq in range(0, 22):
+            seq_folder = join(self.data_path, str(seq).zfill(2))
+
+            # Read Calib
+            self.calibrations.append(self.parse_calibration(join(seq_folder, "calib.txt")))
+
+            # Read times
+            self.times.append(np.loadtxt(join(seq_folder, 'times.txt'), dtype=np.float32))
+
+            # Read poses
+            poses_f64 = self.parse_poses(join(seq_folder, 'poses.txt'), self.calibrations[-1])
+            self.poses.append([pose.astype(np.float32) for pose in poses_f64])
+
+    def parse_calibration(self, filename):
+        """ read calibration file with given filename
+
+            Returns
+            -------
+            dict
+                Calibration matrices as 4x4 numpy arrays.
+        """
+        calib = {}
+
+        calib_file = open(filename)
+        for line in calib_file:
+            key, content = line.strip().split(":")
+            values = [float(v) for v in content.strip().split()]
+
+            pose = np.zeros((4, 4))
+            pose[0, 0:4] = values[0:4]
+            pose[1, 0:4] = values[4:8]
+            pose[2, 0:4] = values[8:12]
+            pose[3, 3] = 1.0
+
+            calib[key] = pose
+
+        calib_file.close()
+
+        return calib
+
+    def parse_poses(self, filename, calibration):
+        """ read poses file with per-scan poses from given filename
+
+            Returns
+            -------
+            list
+                list of poses as 4x4 numpy arrays.
+        """
+        file = open(filename)
+
+        poses = []
+
+        Tr = calibration["Tr"]
+        Tr_inv = np.linalg.inv(Tr)
+
+        for line in file:
+            values = [float(v) for v in line.strip().split()]
+
+            pose = np.zeros((4, 4))
+            pose[0, 0:4] = values[0:4]
+            pose[1, 0:4] = values[4:8]
+            pose[2, 0:4] = values[8:12]
+            pose[3, 3] = 1.0
+
+            poses.append(np.matmul(Tr_inv, np.matmul(pose, Tr)))
+
+        return poses
+
+    def fuse_multi_scan(self, points, pose0, pose):
+
+        # pose = poses[0][idx]
+
+        hpoints = np.hstack((points[:, :3], np.ones_like(points[:, :1])))
+        # new_points = hpoints.dot(pose.T)
+        new_points = np.sum(np.expand_dims(hpoints, 2) * pose.T, axis=1)
+
+        new_points = new_points[:, :3]
+        new_coords = new_points - pose0[:3, 3]
+        # new_coords = new_coords.dot(pose0[:3, :3])
+        new_coords = np.sum(np.expand_dims(new_coords, 2) * pose0[:3, :3], axis=1)
+        new_coords = np.hstack((new_coords, points[:, 3:]))
+
+        return new_coords
+
+    def __getitem__(self, index):
+        raw_data = np.fromfile(self.im_idx[index], dtype=np.float32).reshape((-1, 4))
+        origin_len = len(raw_data)
+        if self.imageset == 'test':
+            annotated_data = np.expand_dims(np.zeros_like(raw_data[:, 0], dtype=int), axis=1)
+        else:
+            annotated_data = np.fromfile(self.im_idx[index].replace('velodyne', 'labels')[:-3] + 'label',
+                                         dtype=np.int32).reshape((-1, 1))
+            annotated_data = annotated_data & 0xFFFF  # delete high 16 digits binary
+            # annotated_data = np.vectorize(self.learning_map.__getitem__)(annotated_data)
+
+        number_idx = int(self.im_idx[index][-10:-4])
+        dir_idx = int(self.im_idx[index][-22:-20])
+
+        pose0 = self.poses[dir_idx][number_idx]
+
+        if number_idx - self.multiscan >= 0:
+
+            for fuse_idx in range(self.multiscan):
+                plus_idx = fuse_idx + 1
+
+                pose = self.poses[dir_idx][number_idx - plus_idx]
+
+                newpath2 = self.im_idx[index][:-10] + str(number_idx - plus_idx).zfill(6) + self.im_idx[index][-4:]
+                raw_data2 = np.fromfile(newpath2, dtype=np.float32).reshape((-1, 4))
+
+                if self.imageset == 'test':
+                    annotated_data2 = np.expand_dims(np.zeros_like(raw_data2[:, 0], dtype=int), axis=1)
+                else:
+                    annotated_data2 = np.fromfile(newpath2.replace('velodyne', 'labels')[:-3] + 'label',
+                                                  dtype=np.int32).reshape((-1, 1))
+                    annotated_data2 = annotated_data2 & 0xFFFF  # delete high 16 digits binary
+
+                raw_data2 = self.fuse_multi_scan(raw_data2, pose0, pose)
+
+                if len(raw_data2) != 0:
+                    raw_data = np.concatenate((raw_data, raw_data2), 0)
+                    annotated_data = np.concatenate((annotated_data, annotated_data2), 0)
+
+        annotated_data = np.vectorize(self.learning_map.__getitem__)(annotated_data)
+
+        data_tuple = (raw_data[:, :3], annotated_data.astype(np.uint8))
+
+        if self.return_ref:
+            data_tuple += (raw_data[:, 3], origin_len) # origin_len is used to indicate the length of target-scan
+
+
+        return data_tuple
+
 
 # load Semantic KITTI class info
 
