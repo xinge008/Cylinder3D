5a7818d90d4c698800cbd405e21c7ab4e8857182
Shynn <tranquynhanh691@gmail.com>
2022-09-24 01:57:25 +0700

    add attention_block_3d.py

---
+++ network/attention_block_3d.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from .segmentator_3d_asymm_spconv import conv1x1
+
+class AttentionBlock(nn.Module):
+    """Attention block with learnable parameters"""
+
+    def __init__(self, F_g, F_l, n_coefficients):
+        """
+        :param F_g: number of feature maps (channels) in previous layer
+        :param F_l: number of feature maps in corresponding encoder layer, transferred via skip connection
+        :param n_coefficients: number of learnable multi-dimensional attention coefficients
+        """
+        super(AttentionBlock, self).__init__()
+
+        self.W_gate = nn.Sequential(
+            conv1x1(F_g, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm1d(n_coefficients)
+        )
+
+        self.W_x = nn.Sequential(
+            conv1x1(F_l, n_coefficients, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm1d(n_coefficients)
+        )
+
+        self.psi = nn.Sequential(
+            conv1x1(n_coefficients, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm1d(1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, gate, skip_connection):
+        """
+        :param gate: gating signal from previous layer
+        :param skip_connection: activation from corresponding encoder layer
+        :return: output activations
+        """
+        g1 = self.W_gate(gate)
+        x1 = self.W_x(skip_connection)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        out = skip_connection * psi
+        return out
+
